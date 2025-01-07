a5c351f43a4159ff27a2c31b6fcd1bad35c75fa6
RyadhLam <lammariryadh@gmail.com>
2025-01-07 19:39:10 +0100

    add

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -14,9 +14,9 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ClerkProvider>
   </React.StrictMode>
 )
--- client/vite.config.js
+++ client/vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
       }
     }
   },
-  base: './',
+  base: '/Removal-IA/',
   define: {
     'process.env': process.env
   }
